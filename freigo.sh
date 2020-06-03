3f087f1972361aed80a0ba5f550c22c3760c9f3c
raydabbah <raydabbah@gmail.com>
2020-06-03 08:48:55 -0400

    don't use --dev by default + add shorthand

--- freigo.sh
+++ freigo.sh
@@ -2,7 +2,7 @@
 gnome-terminal --working-directory=/home/raydabbah/Documents/shifl/ --title=Freigo
 cd /home/raydabbah/Documents/shifl/
 nohup laravel-echo-server start >/dev/null 2>&1 &
-if [ "$1" != "--no-dev" ]; then
+if [[ $1 = "--dev" || $1 = "-d" ]]; then
 yarn dev
 fi
 yarn hot
