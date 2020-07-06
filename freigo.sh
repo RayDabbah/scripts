39c75670e37ace39fc2ba06746bfba3c74a7abbe
raydabbah <raydabbah@gmail.com>
2020-07-06 10:06:39 -0400

    shifl -> freigo

--- freigo.sh
+++ freigo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-gnome-terminal --working-directory=/home/raydabbah/Documents/shifl/ --title=Freigo
-cd /home/raydabbah/Documents/shifl/
+gnome-terminal --working-directory=/home/raydabbah/Documents/freigo/ --title=Freigo
+cd /home/raydabbah/Documents/freigo/
 nohup laravel-echo-server start >/dev/null 2>&1 &
 if [[ $1 = "--dev" || $1 = "-d" ]]; then
 yarn dev
