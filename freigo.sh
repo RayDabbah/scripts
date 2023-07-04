cfa485bf90caf3036e7fcbfc513b89085fb69e9e
raydabbah <raydabbah@gmail.com>
2023-07-03 21:43:10 -0400

    kill horizon and laravel echo server after stopping

--- freigo.sh
+++ freigo.sh
@@ -8,9 +8,16 @@ cd /home/raydabbah/Documents/freigo/ || exit
 
 nohup laravel-echo-server start --force >/dev/null 2>&1 &
 
+LARAVEL_ECHO_SERVER_PID=$!
+
 gnome-terminal --working-directory=$WORKING_DIR --title=Horizon -- nodemon -e php --watch app --exec "php artisan horizon:terminate && php artisan horizon"
 
 if [[ $1 = "--dev" || $1 = "-d" ]]; then
     yarn dev
 fi
+
 yarn hot
+
+kill $(ps -u$(whoami) -o pid,cmd | ps -u1000 -o pid,cmd | awk '/\snode.*nodemon.*php/ {print $1}')
+
+kill $LARAVEL_ECHO_SERVER_PID
